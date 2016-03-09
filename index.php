420953ddb3a4781f5ce357cfcad59c230fc4912f
Robert Perez <ptelerez1@gmail.com>
2016-03-09 18:43:06 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'] . 
+'novice_to_ninja/shoppingcart/includes/magicquotes.php';
+
+$items = array(
+	array('id'=>'1', 'desc'=>'Canadian-Australian Dictionary', 'price'=>24.95),
+ 	array('id'=> '2', 'desc'=>'As-new parachute (never opened)',
+        'price'=>1000),
+    array('id'=> '3', 'desc'=>'Songs of the Goldfish (2CD set)',
+        'price'=>19.99),
+    array('id'=> '4', 'desc'=>'Simply JavaScript (SitePoint)',
+        'price'=>39.95)
+	);
+
+session_start();
+if(!isset($_SESSION['cart']))
+{
+	$_SESSION['cart'] = array();
+}
+
+include 'catalog.html.php';
