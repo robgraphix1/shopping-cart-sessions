50826fe8b8b37b8224ba32c1c8fbaed210f99083
robgraphix1 <ptelerez@cox.net>
2016-03-10 11:41:43 -0500

    test

--- cart.html.php
+++ cart.html.php
@@ -46,7 +46,7 @@
       <p><a href="?">Continue Shopping</a> 
         <?php if(count($_SESSION['cart']) > 0): ?>
           or
-          <input type="submit" name="action" value="Empty cart">
+          <input type="submit" name="action" value="empty cart">
         <?php endif; ?>
       <p>
     </form>
